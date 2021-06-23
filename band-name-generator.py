04734bee871aacea750c5fe120b073d08a8f4258
tosiful <52509622+tosiful@users.noreply.github.com>
2021-06-24 02:13:04 +0600

    Add files via upload

---
+++ band-name-generator.py
@@ -0,0 +1,15 @@
+#1. Create a greeting for your program.
+print("Welcome to the band name generator")
+
+#2. Ask the user for the city that they grew up in.
+city=input("what is your city name?\n")
+
+#3. Ask the user for the name of a pet.
+pet=input("whats your pet name?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("your band name could be-> "+ city +" "+pet)
+
+#https://band-name-generator-end.appbrewery.repl.run/
+
+
